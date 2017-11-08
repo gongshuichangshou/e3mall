e4482caca0dcce7dee1c996b804a5bea5860112c
gongshuichangshou <vh_tk_email@163.com>
2017-11-08 11:51:57 +0800

    上次提交不成功

---
+++ e3-common/src/main/java/cn/e3mall/common/pojo/DatagridResult.java
@@ -0,0 +1,26 @@
+package cn.e3mall.common.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class DatagridResult implements Serializable {
+	private long total;
+	private List rows;
+
+	public List getRows() {
+		return rows;
+	}
+
+	public void setRows(List rows) {
+		this.rows = rows;
+	}
+
+	public long getTotal() {
+		return total;
+	}
+
+	public void setTotal(long total) {
+		this.total = total;
+	}
+
+}
