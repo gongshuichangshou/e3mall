b3b5bfbaab01205543fb50ab8e29ddca9866aaa8
gongshuichangshou <vh_tk_email@163.com>
2017-11-08 11:48:21 +0800

    提交后台管理的表现层manager-web

---
+++ e3-manager-web/src/main/java/cn/e3mall/controller/PageController.java
@@ -0,0 +1,18 @@
+package cn.e3mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/{page}")
+	public String result(@PathVariable String page) {
+		return page;
+	}
+}
---
+++ e3-manager-web/src/test/java/cn/e3mall/testdubbo/PageHelperTest.java
@@ -0,0 +1,10 @@
+package cn.e3mall.testdubbo;
+
+import org.junit.Test;
+
+public class PageHelperTest {
+	@Test
+	public void testPage()throws Exception {
+    
+	}
+}
