c3c4b5c34fd3dba6de221e04951bca3e190c0f7c
gongshuichangshou <vh_tk_email@163.com>
2017-12-17 20:29:55 +0800

    在Test分支上面测试

--- e3-managerment/e3-manager-service/src/test/java/cn/e3mall/test/Hello.java
+++ e3-managerment/e3-manager-service/src/test/java/cn/e3mall/test/Hello.java
@@ -8,6 +8,6 @@ public class Hello {
 
     public void main(String[] args) {
         System.out.println("你好");
-        System.out.println("测试了好几次都不成功");
+        System.out.println("再一次在Test分支修改");
     }
 }
