5dd862adf5c80ef96ea2e844b1b63a86459ea928
gongshuichangshou <vh_tk_email@163.com>
2017-12-17 23:04:21 +0800

    刚刚把master分支合并到了Test分支,而且同时做了修改

--- e3-manager-web/src/test/java/cn/e3mall/testdubbo/PageHelperTest.java
+++ e3-manager-web/src/test/java/cn/e3mall/testdubbo/PageHelperTest.java
@@ -10,5 +10,12 @@ public class PageHelperTest {
         System.out.println("你好");
         System.out.println("丰额阿哥啊发");
         System.out.println("dajaohjoigaoigjoaigoi");
+        System.out.println("你好阿哥啊个仍啊");
+        System.out.println("丰额阿哥啊发");
+        System.out.println("丰额阿哥啊发");
+        System.out.println("丰额阿哥啊发");
+        System.out.println("丰额阿哥啊发");
+        System.out.println("丰额阿哥啊发");
+        System.out.println("丰额阿哥啊发");
     }
 }
