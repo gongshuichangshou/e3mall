7d05715935e9df2a6faf5a5ea2043ba5ddbd2f1e
gongshuichangshou <vh_tk_email@163.com>
2017-12-17 20:14:14 +0800

    测试idea

--- e3-manager-web/src/test/java/cn/e3mall/testdubbo/PageHelperTest.java
+++ e3-manager-web/src/test/java/cn/e3mall/testdubbo/PageHelperTest.java
@@ -3,8 +3,8 @@ package cn.e3mall.testdubbo;
 import org.junit.Test;
 
 public class PageHelperTest {
-	@Test
-	public void testPage()throws Exception {
-    
-	}
+    @Test
+    public void testPage() throws Exception {
+        System.out.println("你好");
+    }
 }
